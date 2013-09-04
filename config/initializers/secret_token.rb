7baecbf62c48f52cb74b9cde5c586ede58eeea1c
Jakub Truneček <jakub.trunecek@gmail.com>
2013-09-04 16:07:32 +0200

    Brand new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vetreserv::Application.config.secret_key_base = 'eb446ff5a451935181d4b6b9d08f33c26e27cc6c7d545439d98d2bf1ef00efb52911b396b8a82b8c0a0f1d3bf3f1e50fa9df331458479ece63a684333dcde763'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vetreserv::Application.config.session_store :cookie_store, key: '_vetreserv_session'
